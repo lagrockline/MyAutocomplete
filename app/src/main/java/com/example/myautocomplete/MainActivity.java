88f16594de8e8d75c46b26256218ebf5cf9ef1d8
lagrockline <sebastien.guinle@wanadoo.fr>
2022-05-05 12:43:00 +0200

    Ajout variables ctx et addresslist

--- app/src/main/java/com/example/myautocomplete/MainActivity.java
+++ app/src/main/java/com/example/myautocomplete/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.myautocomplete;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AutoCompleteTextView textView = findViewById(R.id.autocompletePlaces);
-    }
+
+        Context ctx = getApplicationContext();
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        ArrayList<String>addressList = new ArrayList<>();
+
+    }//FIN onCreate
 }
